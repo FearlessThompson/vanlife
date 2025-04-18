d1e007575b0d2a481f17780304b1812a2083d6ed
FearlessThompson <fearlessthompson@protonmail.com>
2025-04-18 12:56:57 +0100

    Added Viewport meta

--- index.jsx
+++ index.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
     <BrowserRouter>
       <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <Link className="site-logo" to="/">
+          #VanLife
+        </Link>
         <nav>
           <Link to="/about">About</Link>
         </nav>
@@ -18,9 +22,7 @@ function App() {
         <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
